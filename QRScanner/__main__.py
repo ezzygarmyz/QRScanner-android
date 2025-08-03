fe4395312a703f9f64c5dfa1b3e48a72cc530153
ezzygarmyz <dragkhai@gmail.com>
2025-08-03 17:28:36 +0100

    add torch/beep toggles and auto-copy to clipboard

--- QRScanner/__main__.py
+++ QRScanner/__main__.py
@@ -1,13 +1,17 @@
 
-from java import dynamic_proxy
+import asyncio
+
+from java import dynamic_proxy, cast
 from java.util import Arrays
 from java.lang import Runnable
+from android.content import Context, ClipData, ClipboardManager
+from android.widget import Toast
 from android.content.res import Configuration
 from androidx.activity.result import ActivityResultCallback
 from com.journeyapps.barcodescanner import ScanOptions, ScanContract
 from org.beeware.android import MainActivity, IPythonApp, PortraitCaptureActivity
 
-from toga import App, MainWindow, Box, Label, Button
+from toga import App, MainWindow, Box, Label, Button, Switch
 from toga.style.pack import Pack
 from toga.constants import COLUMN, CENTER, BOLD
 from toga.colors import rgb, WHITE, GREEN, BLACK, YELLOW
@@ -23,15 +27,49 @@ class RunnableProxy(dynamic_proxy(Runnable)):
 
 
 class QRCallbackProxy(dynamic_proxy(ActivityResultCallback)):
-    def __init__(self, callback):
+    def __init__(self, scanner):
         super().__init__()
-        self._callback = callback
+        self.scanner = scanner
 
     def onActivityResult(self, result):
         if result and result.getContents():
             contents = result.getContents()
-            if callable(self._callback):
-                self._callback(contents)
+            self.scanner._set_result(contents)
+        else:
+            self.scanner._set_result(None)
+    
+
+
+class QRScanner:
+    def __init__(self):
+        self.activity = MainActivity.singletonThis
+        self._launcher = None
+        self._future = None
+
+        scan_contract = ScanContract()
+        callback_proxy = QRCallbackProxy(self)
+        self._launcher = self.activity.registerForActivityResult(scan_contract, callback_proxy)
+
+    async def start_scan(self, beep=False, torch=False):
+        if self._launcher is None:
+            raise RuntimeError("QRScanner callback must be set before scanning")
+
+        self._future = asyncio.get_event_loop().create_future()
+
+        options = ScanOptions()
+        options.setPrompt("Scan a QR Code")
+        options.setBeepEnabled(beep)
+        options.setCaptureActivity(PortraitCaptureActivity)
+        options.setDesiredBarcodeFormats(Arrays.asList("QR_CODE"))
+        options.setTorchEnabled(torch)
+
+        self.activity.runOnUiThread(RunnableProxy(lambda: self._launcher.launch(options)))
+        return await self._future
+
+    def _set_result(self, contents):
+        if self._future and not self._future.done():
+            self._future.set_result(contents)
+
 
 
 class PythonAppProxy(dynamic_proxy(IPythonApp)):
@@ -49,30 +87,6 @@ class PythonAppProxy(dynamic_proxy(IPythonApp)):
             except Exception as e:
                 print("Back callback error:", e)
         return False
-    
-
-
-class QRScanner:
-    def __init__(self, callback):
-        self.activity = MainActivity.singletonThis
-        self._launcher = None
-
-        scan_contract = ScanContract()
-        callback_proxy = QRCallbackProxy(callback)
-
-        self._launcher = self.activity.registerForActivityResult(scan_contract, callback_proxy)
-
-    def start_scan(self):
-        if self._launcher is None:
-            raise RuntimeError("QRScanner callback must be set before scanning")
-
-        options = ScanOptions()
-        options.setPrompt("Scan a QR Code")
-        options.setBeepEnabled(False)
-        options.setCaptureActivity(PortraitCaptureActivity)
-        options.setDesiredBarcodeFormats(Arrays.asList("QR_CODE"))
-
-        self.activity.runOnUiThread(RunnableProxy(lambda: self._launcher.launch(options)))
 
 
 
@@ -81,9 +95,7 @@ class QRScannerGUI(MainWindow):
         super().__init__()
 
         version = self.app.version
-        self._qr_scanner = QRScanner(
-            callback=self.on_qr_scanned
-        )
+        self._qr_scanner = QRScanner()
 
         ui_mode = self.get_ui_mode()
         if ui_mode == "dark":
@@ -117,6 +129,36 @@ class QRScannerGUI(MainWindow):
             )
         )
 
+        self.widgets_box = Box(
+            style=Pack(
+                direction = COLUMN,
+                background_color = background_color,
+                flex = 1
+            )
+        )
+
+        self.torch_switch = Switch(
+            text="Torch :",
+            value=False,
+            style=Pack(
+                color = color,
+                background_color = background_color,
+                font_size = 14,
+                padding= (15,20,0,20)
+            )
+        )
+
+        self.beep_switch = Switch(
+            text="Beep :",
+            value=False,
+            style=Pack(
+                color = color,
+                background_color = background_color,
+                font_size = 14,
+                padding= (10,20,0,20)
+            )
+        )
+
         self.scan_button = Button(
             text="Scan QR",
             style=Pack(
@@ -131,21 +173,41 @@ class QRScannerGUI(MainWindow):
 
         self.content = self.main_box
         self.main_box.add(
-            self.scan_button,
+            self.widgets_box,
             self.app_version
+        )
+        self.widgets_box.add(
+            self.beep_switch,
+            self.torch_switch,
+            self.scan_button
         )
 
 
     
     def scan_qr(self, button):
-        self._qr_scanner.start_scan()
+        beep = self.beep_switch.value
+        torch = self.torch_switch.value
+        asyncio.ensure_future(self.handle_scan(beep, torch))
 
 
-    def on_qr_scanned(self, contents):
-        self.info_dialog(
-            title="QR Code Scanned",
-            message=f"Result :\n{contents}"
-        )
+    async def handle_scan(self, beep, torch):
+        result = await self._qr_scanner.start_scan(beep, torch)
+        if result:
+
+            def on_result(widget, result):
+                if result is None:
+                    context = MainActivity.singletonThis.getApplicationContext()
+                    clipboard = context.getSystemService(Context.CLIPBOARD_SERVICE)
+                    clipboard_manager = cast(ClipboardManager, clipboard)
+                    clip = ClipData.newPlainText("QR Code", result)
+                    clipboard_manager.setPrimaryClip(clip)
+                    Toast.makeText(context, "Copied to clipboard", Toast.LENGTH_SHORT).show()
+
+            self.info_dialog(
+                title="QR Code Scanned",
+                message=f"Result:\n{result}",
+                on_result=on_result
+            )
 
     def get_ui_mode(self):
         try:
@@ -192,7 +254,7 @@ def main():
         app_id = "com.qrscanner",
         home_page = "https://example.com",
         author = "BTCZCommunity",
-        version = "1.0.0"
+        version = "1.1.0"
     )
     app.main_loop()
 
